393e04f2969b9a7b093880320973c08e8724fa28
Jay Stanley <jstanley734@gmail.com>
2017-03-11 11:08:37 -0500

    Add javascript

--- ZenPacks/jstanley/EventReplay/replayApi.py
+++ ZenPacks/jstanley/EventReplay/replayApi.py
@@ -12,4 +12,23 @@ from Products.ZenUtils.Ext import DirectRouter, DirectResponse
 from Products.Zuul.facades import ZuulFacade
 from Products.Zuul.interfaces import IFacade
 
+class IEventReplayFacade(IFacade):
+    def replay(self, events):
+        """Replay an event from redis using eventReplayId(s)"""
 
+
+class EventReplayFacade(ZuulFacade):
+    implements(IEventReplayFacade)
+
+    def replay(self, events):
+       import pdb; pdb.set_trace()
+
+
+class EventReplayRouter(DirectRouter):
+    def _getFacade(self):
+        return Zuul.getFacade('eventreplay', self.context)
+
+    def replay(self, events):
+        facade = self._getFacade()
+        data = facade.replay(events)
+        return DirectResponse.succeed(data=data)
---
+++ ZenPacks/jstanley/EventReplay/resources/js/eventReplay.js
@@ -0,0 +1,56 @@
+Ext.onReady(function(){
+Ext.ns("Zenoss.CheckTrigger.dialogs")
+
+var gridId = "events_grid";
+
+function getSelectedEventReplayIds() {
+    var grid = Ext.getCmp(gridId),
+        selected = grid.selModel.getSelection();
+    return Ext.pluck(Ext.Array.map(selected, function(value, ind, array) { return value.getData() }), 'eventReplayId');
+}
+function getSelectedEvents() {
+    var grid = Ext.getCmp(gridId);
+    return grid.getSelectionModel().getSelection();
+}
+
+var addEventReplayButton = function(event_grid) {
+    var eventsToolbar = event_grid.tbar;
+
+    var createEventReplayButton = new Ext.Button({
+        id: Ext.id('','create-eventreplay-button'),
+        text: 'Event Replay',
+        menu: {
+            items: [{
+                text: 'Replay event',
+                tooltip: 'Send raw event back through the system',
+                handler: function() {
+                    Zenoss.remote.EventReplayRouter.replay({ events: getSelectedEvents() });
+                },
+            }]
+        }
+    });
+
+    event_grid.child(0).add(createEventReplayButton);
+    return createEventReplayButton;
+};
+
+var setupEventReplayButton = function(event_grid) {
+    var new_grid = Ext.getCmp('events_grid');
+    var createEventReplayButton = addEventReplayButton(new_grid);
+    new_grid.on('selectionchange', function(selectionmodel) {
+        var newDisabledValue = !selectionmodel.hasSelection() && selectionmodel.selectState !== 'All',
+            history_combo = Ext.getCmp('history_combo'),
+            archive = Ext.isDefined(history_combo) ? history_combo.getValue() === 1 : false;
+        if (archive) {
+            createEventReplayButton.setDisabled(true);
+        } else {
+            createEventReplayButton.setDisabled(newDisabledValue);
+        }
+    });
+    new_grid.on('recreateGrid', setupEventReplayButton)
+}
+
+Ext.ComponentMgr.onAvailable('events_grid', setupEventReplayButton);
+
+});
+
